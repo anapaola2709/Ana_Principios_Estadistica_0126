b69aaa3a655b072a3b2a7a367f4f9c684d55e164
anapaola2709 <98105076+anapaola2709@users.noreply.github.com>
2022-05-18 08:16:33 -0500

    Update

--- TAREA/Tarea 1.R
+++ TAREA/Tarea 1.R
@@ -1,3 +1,46 @@
 Tarea 
 + 02/02/2022
 +Tarea 1
+
+#Parte 2
+
+#Problema 1__________________________________________________________________________
+
+Pinus <- 3140
+Mezquite <- 1453
+Encinos <- 450
+Teka <- 1200
+Juniperos <- 720
+
+superficie <- c(Pinus, Mezquite, Encinos, Teka, Juniperos)
+
+barplot(superficie)
+sort(superficie)
+barplot(sort(superficie, decreasing = TRUE))
+
+mean(superficie)
+#1392.6
+
+#Problema 2______________________________________________________________________________
+
+germinacion <- c( 4, 1, 6, 2, 4, 2, 4, 2, 4, 6, 3, 5, 3, 2, 5, 4, 0, 5, 4, 2, 4, 5, 3, 5, 3, 5, 4, 3, 6, 2)
+
+mean(germinacion)
+#3.6
+
+sd(superficie)
+#1052.976
+
+#Problema 3_____________________________________________________________________________________
+
+Prosopis <- c(1:25)
+altura <- c(38, 14, 44, 11, 9, 21, 39, 28, 41, 4, 35, 24, 36, 12, 20, 31, 24, 25, 10, 21, 11, 36, 37, 20, 26)
+
+mean(altura)
+#24.68
+
+sd(altura, na.rm = FALSE)
+#11. 54599
+
+
+
